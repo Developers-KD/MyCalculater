ab1d1d58a3e17ef88ce7e364920317da6e3d584d
KOUSHIK007321 <52344741+KOUSHIK007321@users.noreply.github.com>
2020-04-05 08:29:46 +0530

    first commit

---
+++ script/script.js
@@ -0,0 +1,112 @@
+function getHistory() {
+    return document.getElementById("history-value").innerText;
+}
+function printHistory(num) {
+    return document.getElementById("history-value").innerText = num;
+}
+function getOutput() {
+    return document.getElementById("output-value").innerText;
+}
+function printOutput(num) {
+    if (num == "") {
+        document.getElementById("output-value").innerText = num;
+    }
+    else {
+        document.getElementById("output-value").innerText = getFormattedNumber(num);
+    }
+}
+
+
+function getFormattedNumber(num) {
+    if (num == "-") {
+        return "";
+    }
+    let str = Number(num);
+    let value = str.toLocaleString("en");
+    return value;
+}
+
+function reverseNumberFormat(num) {
+    return Number(num.replace(/,/g, ''));
+}
+
+
+
+
+var operator = document.getElementsByClassName("operator");
+for (let i = 0; i < operator.length; i++) {
+    operator[i].addEventListener('click', function () {
+        if (this.id == "clear") {
+            printHistory("");
+            printOutput("");
+        }
+        else if (this.id == 'delete') {
+            let output = reverseNumberFormat(getOutput()).toString();
+            if (output) {
+                output = output.substr(0, output.length - 1);
+                printOutput(output);
+            }
+        }
+        else {
+            let output = getOutput();
+            let history = getHistory();
+            // if(output = "" && history!= ""){
+            //     if(isNaN(history[history.length-1])){
+            //         history = history.substring(0,history.length-1);
+            //     }
+            // }
+            if (output != "") {
+                output = output == "" ?
+                    output : reverseNumberFormat(output);
+                history = history + output;
+                if (this.id == "=") {
+                    let result = eval(history);
+                    printOutput(result);
+                    printHistory("");
+                }
+                else {
+                    history = history + this.id;
+                    printHistory(history);
+                    printOutput("");
+                }
+            }
+        }
+    });
+}
+
+var number = document.getElementsByClassName("number");
+for (let i = 0; i < number.length; i++) {
+    number[i].addEventListener('click', function () {
+        let output = reverseNumberFormat(getOutput());
+        if (output != NaN) {
+            output = output + this.id;
+            printOutput(output);
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
